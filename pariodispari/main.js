be38feba8fec27baddf4382d9104609cb1a14a7d
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-08 13:37:29 +0200

    COMPUTERNUMBER E SUM

--- pariodispari/main.js
+++ pariodispari/main.js
@@ -1,6 +1,7 @@
 // utente inserisce un numero da 1 a 5
 const userNumber = +(prompt('inserisci un numero'));// trasformo il numero dà string à number
-console.log(userNumber);
+console.log('Numero utente',userNumber);
+
 // utente sceglie pari o dispari
 
 let userChoice = prompt('inserisci pari o dispari');
@@ -9,14 +10,17 @@ console.log(userChoice);
 
 
 function gtnNumberRandom (num1,num2){
-    const result = Math.floor(Math.random() * num2)- num1;
+    const result = Math.floor(Math.random() * num2) + num1;
     return  result;
 }
-console.log (gtnNumberRandom(1,10));
 
+const computerNumber = gtnNumberRandom(1,5);
+console.log('Numero computer',computerNumber);  // genero un numero random da 1 a 5 per il computer e lo salvo in unà variabile
 
-// genero un numero random da 1 a 5 per il computer e lo salvo in unà variabile
+sum = computerNumber + userNumber;
 // sommo il numero del computer èil numero dell'utente
+console.log (sum);
+
 // creo una funzione che stabilisca se un numero e pari o e dispari
 // se il risultato della funzione e pari e l'utente ha scelto pari allora a vinto l'utente
 // altrimenti a vinto il computer.
